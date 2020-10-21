3d44993e929b3ba60603004a5aebe7e32b20b12b
seedubjay <cwj@dhcp-10-249-79-225.eduroam.wireless.private.cam.ac.uk>
2020-10-21 02:36:44 +0100

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+flash microSD card
+
+in `sudo raspi-config`, set hostname to rpiXX
+
+add `/etc/wpa_supplicant/wpa_supplicant.conf`
+'''
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=0
+country=GB
+
+network={
+	ssid="eduroam"
+	key_mgmt=WPA-EAP
+	eap=PEAP
+	pairwise=CCMP TKIP
+	phase2="auth=MSCHAPV2"
+	
+	anonymous_identity="@cam.ac.uk"
+	ca_cert="/etc/ssl/certs/QuoVadis_Root_CA_2_G3.pem"
+
+	identity="XXX+rpiXX@cam.ac.uk"
+	password="XXXXXXXXXX"
+}
+'''
+with token from [https://tokens.csx.cam.ac.uk/](UIS)
+
---
+++ setup.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/sh
+
+wget http://www.virtualhere.com/sites/default/files/usbserver/vhusbdarm -O /usr/sbin/vhusbdarm
+chmod +x /usr/sbin/vhusbdarm
+
+echo "
+[Unit]
+Description=Announce IP
+Requires=networking.service
+After=networking.service
+[Service]
+ExecStart=curl -X POST https://frozen-island-91924.herokuapp.com/$(hostname)/$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+Type=oneshot
+" > /etc/systemd/system/announce-ip.service
+
+echo "
+[Unit]
+Description=Announce IP Timer
+[Timer]
+OnUnitActiveSec=10s
+OnBootSec=10s
+
+[Install]
+WantedBy=timers.target
+" > /etc/systemd/system/announce-ip.timer
+
+systemctl disable virtualhere
+systemctl stop virtualhere
+
+echo "
+[Unit]
+Description=VirtualHere USB Sharing
+Requires=networking.service
+After=networking.service
+[Service]
+ExecStart=/usr/sbin/vhusbdarm -c /usr/vhusbd.conf
+Type=idle
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/virtualhere.service
+
+echo "
+ServerName=$HOSTNAME$
+" > /etc/vhusbd.conf
+
+systemctl daemon-reload
+
+systemctl enable announce-ip.timer
+systemctl start announce-ip.timer
+
+systemctl enable virtualhere
+systemctl start virtualhere
